3e44b0bede3c969387a658d007f247bd4c32909d
unknown <ngordon@mail.ru>
2024-03-03 16:57:13 +0200

    Changes

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public int calculate(int weightInKg, double heightInMeters) {
-        double bmi = weightInKg/(heightInMeters*heightInMeters); {
+        double bmi = weightInKg / (heightInMeters * heightInMeters);
+        {
             return (int) bmi;
 
         }
